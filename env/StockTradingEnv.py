f57e5727a6bf05e68a971b945624222f8b87d3c2
Pjus <wnstjd117@naver.com>
2020-08-12 12:42:00 +0900

    update env

--- env/StockTradingEnv.py
+++ env/StockTradingEnv.py
@@ -4,6 +4,7 @@ import gym
 from gym import spaces
 import pandas as pd
 import numpy as np
+from collections import deque
 
 from render.StockTradingGraph import StockTradingGraph
 
@@ -11,11 +12,11 @@ MAX_ACCOUNT_BALANCE = 2147483647
 MAX_NUM_SHARES = 2147483647
 MAX_SHARE_PRICE = 5000
 MAX_OPEN_POSITIONS = 5
-MAX_STEPS = 20000
+
 
 INITIAL_ACCOUNT_BALANCE = 10000
 
-LOOKBACK_WINDOW_SIZE = 40
+LOOKBACK_WINDOW_SIZE = 10
 
 
 def factor_pairs(val):
@@ -29,6 +30,7 @@ class StockTradingEnv(gym.Env):
 
     def __init__(self, df):
         super(StockTradingEnv, self).__init__()
+        self.MAX_STEPS = len(df)
 
         self.df = self._adjust_prices(df)
         self.reward_range = (0, MAX_ACCOUNT_BALANCE)
@@ -39,7 +41,7 @@ class StockTradingEnv(gym.Env):
 
         # Prices contains the OHCL values for the last five prices
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(5, LOOKBACK_WINDOW_SIZE + 2), dtype=np.float16)
+            low=0, high=1, shape=(len(self.df.columns) + 4, LOOKBACK_WINDOW_SIZE), dtype=np.float16)
 
     def _adjust_prices(self, df):
         adjust_ratio = df['Adj Close'] / df['Close']
@@ -52,32 +54,37 @@ class StockTradingEnv(gym.Env):
         return df
 
     def _next_observation(self):
-        frame = np.zeros((5, LOOKBACK_WINDOW_SIZE + 1))
 
-        # Get the stock data points for the last 5 days and scale to between 0-1
-        np.put(frame, [0, 4], [
-            self.df.loc[self.current_step: self.current_step +
-                        LOOKBACK_WINDOW_SIZE, 'Open'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        LOOKBACK_WINDOW_SIZE, 'High'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        LOOKBACK_WINDOW_SIZE, 'Low'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        LOOKBACK_WINDOW_SIZE, 'Close'].values / MAX_SHARE_PRICE,
-            self.df.loc[self.current_step: self.current_step +
-                        LOOKBACK_WINDOW_SIZE, 'Volume'].values / MAX_NUM_SHARES,
-        ])
+        frame = np.zeros((11,10))
 
-        # Append additional data and scale each value to between 0-1
-        obs = np.append(frame, [
-            [self.balance / MAX_ACCOUNT_BALANCE],
-            [self.max_net_worth / MAX_ACCOUNT_BALANCE],
-            [self.shares_held / MAX_NUM_SHARES],
-            [self.cost_basis / MAX_SHARE_PRICE],
-            [self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE)],
-        ], axis=1)
+        opendeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        highdeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        lowdeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        closedeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        ajdeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        voldeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        bladeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        netdeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        shadeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        costdeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
+        totaldeq = deque(maxlen=LOOKBACK_WINDOW_SIZE)
 
-        return obs
+        opendeq.append(self.df.loc[self.current_step, 'Open'] / MAX_SHARE_PRICE)
+        highdeq.append(self.df.loc[self.current_step, 'High'] / MAX_SHARE_PRICE)
+        lowdeq.append(self.df.loc[self.current_step, 'Low'] / MAX_SHARE_PRICE)
+        closedeq.append(self.df.loc[self.current_step, 'Close'] / MAX_SHARE_PRICE)
+        ajdeq.append(self.df.loc[self.current_step, 'Adj Close'] / MAX_SHARE_PRICE)
+        voldeq.append(self.df.loc[self.current_step, 'Volume'] / MAX_SHARE_PRICE)
+        bladeq.append(self.balance / MAX_ACCOUNT_BALANCE)
+        netdeq.append(self.max_net_worth / MAX_ACCOUNT_BALANCE)
+        shadeq.append(self.shares_held / MAX_NUM_SHARES)
+        costdeq.append(self.cost_basis / MAX_SHARE_PRICE)
+        totaldeq.append(self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE))
+
+        if self.current_step > 10:
+            obs = np.array([opendeq, highdeq, lowdeq, closedeq, ajdeq, voldeq, bladeq, netdeq, shadeq, costdeq, totaldeq])
+            return obs
+        return frame
 
     def _take_action(self, action):
         current_price = random.uniform(
@@ -133,12 +140,13 @@ class StockTradingEnv(gym.Env):
         self._take_action(action)
 
         self.current_step += 1
+        if self.current_step > self.df.index[-1]:
+            self.current_step = self.df.index[-1]
 
-        delay_modifier = (self.current_step / MAX_STEPS)
+        delay_modifier = (self.current_step / self.MAX_STEPS)
 
         reward = self.balance * delay_modifier + self.current_step
-        done = self.net_worth <= 0 or self.current_step >= len(
-            self.df.loc[:, 'Open'].values)
+        done = self.net_worth <= 0 or self.current_step >= len(self.df)
 
         obs = self._next_observation()
 
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import pandas_datareader as pdr
 import gym
+
+from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
+# from env.testEnv import StockTradingEnv
 from env.StockTradingEnv import StockTradingEnv
 
 import pandas as pd
-import joblib
-import itertools
-
 model = PPO2.load("./model/stock_RL2")
 
 ticker = 'AAPL'
@@ -19,10 +19,18 @@ df['Date'] = df.index
 df.index = range(len(df))
 df = df.sort_values('Date')
 df['Date'] = df['Date'].astype('str')
+print(len(df.columns))
+
+print(df.drop(['Date'], axis=1).iloc[1].to_numpy())
+
 
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: StockTradingEnv(df)])
 
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=100)
+
+print(env.observation_space)
 obs = env.reset()
 for i in range(len(df['Date'])):
     action, _states = model.predict(obs)
@@ -30,6 +38,7 @@ for i in range(len(df['Date'])):
     # print("action type : ",type(action))
 
     obs, rewards, done, info = env.step(action)
+    # print("obs : ", obs)
     print("Date : ", info[0]['date'])
     print("Trade : ", info[0]['trade'])
     print("Total_Net : ", info[0]['Net_worth'])
